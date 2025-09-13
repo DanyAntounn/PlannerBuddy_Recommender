e1dc22b8699e1145c47934fcf5528485297d2770
DanyAntounn <143404761+DanyAntounn@users.noreply.github.com>
2025-09-13 22:37:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List, Dict
 from recommender import recommend_food_places, recommend_activity_places
 
 app = FastAPI()
+
+class FoodRequest(BaseModel):
+    user_profile: Dict
+    food_places: List[Dict]
+
+class ActivityRequest(BaseModel):
+    user_profile: Dict
+    activity_places: List[Dict]
 
 @app.get("/")
 def root():
     return {"message": "Recommender API is running"}
 
 @app.post("/recommend/food")
-def food_recommendation(user_profile: dict = Body(...), food_places: list = Body(...)):
-    return recommend_food_places(user_profile, food_places)
+def food_recommendation(request: FoodRequest):
+    return recommend_food_places(request.user_profile, request.food_places)
 
 @app.post("/recommend/activity")
-def activity_recommendation(user_profile: dict = Body(...), activity_places: list = Body(...)):
-    return recommend_activity_places(user_profile, activity_places)
+def activity_recommendation(request: ActivityRequest):
+    return recommend_activity_places(request.user_profile, request.activity_places)
